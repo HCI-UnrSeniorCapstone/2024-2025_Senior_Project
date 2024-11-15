bc2ac0f547c81e74037debe8e5e867204b746a83
Jarvis-Nicholas <njarvis@unr.edu>
2024-11-14 23:06:03 -0800

    basic server flask ping

---
+++ server_backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# creating app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS w/ specific routes
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# Basic ping
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"message": "Pong!"})
+
+# CHANGE THE HOST AND DEBUG WHEN PRODUCTION TIME
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
