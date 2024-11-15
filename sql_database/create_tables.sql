b493fbf85590b854185b09f4ee6c27d93e50c4f8
Jarvis-Nicholas <njarvis@unr.edu>
2024-11-15 15:10:32 -0800

    fixed Null vs Not Null

--- sql_database/create_tables.sql
+++ sql_database/create_tables.sql
@@ -9,23 +9,25 @@ CREATE TABLE user (
 CREATE TABLE admin_user (
     admin_user_id INT,
     user_id INT,
-    PRIMARY KEY (admin_user_id, user_id),
+    PRIMARY KEY (admin_user_id, user_id) ,
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 CREATE TABLE task (
-    task_id INT PRIMARY KEY,
-    task_name VARCHAR(255),
+    task_id INT PRIMARY KEY ,
+    task_name VARCHAR(255) NOT NULL,
     task_description VARCHAR(255),
     duration DECIMAL(6, 3)
 );
 CREATE TABLE measurement_type (
     measurement_type_id INT PRIMARY KEY,
-    measurement_type_name VARCHAR(255)
+    measurement_type_name VARCHAR(255) NOT NULL
 );
+
+-- If a task has no measurement type, it won't be in this table
 CREATE TABLE task_measurement (
     task_id INT,
     measurement_type_id INT,
-    PRIMARY KEY (task_id, measurement_type_id),
+    PRIMARY KEY (task_id, measurement_type_id) ,
     FOREIGN KEY (task_id) REFERENCES task(task_id),
     FOREIGN KEY (measurement_type_id) REFERENCES measurement_type(measurement_type_id)
 );
@@ -33,33 +35,41 @@ CREATE TABLE factor (
     factor_id INT PRIMARY KEY,
     factor_description VARCHAR(255)
 );
+
+CREATE TABLE study_design_type(
+    study_design_type_id INT PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE study (
     study_id INT PRIMARY KEY,
-    study_name VARCHAR(255),
+    study_name VARCHAR(255) NOT NULL,
     study_description VARCHAR(255),
-    expected_participants INT
+    study_design_type INT NOT NULL,
+    expected_participants INT NOT NULL,
+    FOREIGN KEY (study_design_type) REFERENCES study_design_type(study_design_type_id)
 );
---Read, Write, Read/Write. No None cuz they just wouldn't be in the table
+-- Read, Write, Read/Write. No None cuz they just wouldn't be in the table
 CREATE TABLE study_user_access_type (
     study_user_access_type_id INT PRIMARY KEY,
-    description VARCHAR(255)
+    description VARCHAR(255) NOT NULL
 );
---All users who have access to a study and their permission types
+-- All users who have access to a study and their permission types
 CREATE TABLE study_user_access (
     user_id INT,
     study_id INT,
-    access_type INT,
+    access_type INT NOT NULL,
     PRIMARY KEY (user_id, study_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (study_id) REFERENCES study(study_id),
     FOREIGN KEY (access_type) REFERENCES study_user_access_type(study_user_access_type_id)
 );
---This assumes every task has the same factors. need to double-check
+-- This assumes every task has the same factors. need to double-check
 CREATE TABLE study_task_factor (
     study_id INT,
     task_id INT,
     factor_id INT,
-    PRIMARY KEY (study_id, task_id),
+    PRIMARY KEY (study_id, task_id, factor_id),
     FOREIGN KEY (study_id) REFERENCES study(study_id),
     FOREIGN KEY (task_id) REFERENCES task(task_id),
     FOREIGN KEY (factor_id) REFERENCES factor(factor_id)
