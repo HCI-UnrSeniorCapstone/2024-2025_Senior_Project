ccaf3a35c3888f466bb0c4261af3e14d74e77903
Jarvis-Nicholas <njarvis@unr.edu>
2024-11-22 15:02:08 -0800

    restructured main code into new file structure

--- server_backend/app/routes/studies.py
+++ server_backend/app/routes/studies.py
@@ -10,38 +10,22 @@ bp = Blueprint('studies', __name__)
     # ./frontend/public/demo2.json
 @bp.route("/create_study", methods=["POST", "GET"])
 def create_study():
-    task_name = []
-    task_duration = []
-    task_measurements = []
-    user_Task = []
-
-    # num will be whatever we set it as in vue
-    # default for now will be on 10
     submissionData = request.get_json()
-    default_tasks = submissionData.get('tasks', [])
-    study_name, study_desc, study_design, people_count = get_study_detail(
-        submissionData)
-
-    # app.logger.debug(submissionData)
-
-    # randominzing dataset for tasks (come back to this once we have factors added to the frontend)
-    rand_tasks = sorted(default_tasks, key=lambda x: random.random())
-
-    for i in range(len(rand_tasks)):
-        task_name.append(rand_tasks[i]['taskName'])
-        task_duration.append(int(rand_tasks[i]['taskDuration']))
-        task_measurements.append(rand_tasks[i]['measurementOptions'])
-
-    # checks to see what rand_tasks were selected
-    for task_amount in range(len(task_measurements)):
-        rand_tasks = set_available_features(task_measurements[task_amount])
-        user_Task.append(rand_tasks)
 
     # This is reading and writing to a json file
-        # https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
+    # https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
     json_object = json.dumps(submissionData, indent=4)
     with open(f'../frontend/public/demo2.json', 'w') as f:
         f.write(json_object)
-    # RECORDS EXPERIMENTS
-    # get_measurments(user_Task, task_name, task_duration)
-    return "finished"
+
+    return 'finished'
+
+@bp.route("/get_data", methods=["POST", "GET"])
+def get_data():
+
+    # https://www.geeksforgeeks.org/read-json-file-using-python/
+    # gets the json data from the folder (db in the future)
+    with open('../frontend/public/demo2.json', 'r') as file:
+        data = json.load(file)
+
+    return data
