614ebd88cc3aebeab7701a398418282ef443a62b
Jarvis-Nicholas <97933530+Jarvis-Nicholas@users.noreply.github.com>
2025-02-14 00:47:57 -0800

    Update apply_python_formatter.yml (#118)
    * Update apply_python_formatter.yml
    
    * Apply Black code formatting to: Update apply_python_formatter.yml
    
    ---------
    
    Co-authored-by: sharedhci <sharedhci@gmail.com>

--- local_backend/app.py
+++ local_backend/app.py
@@ -1,7 +1,7 @@
-'''
+"""
 Used this tutorial for setting up Vue and Flask together
 https://testdriven.io/blog/developing-a-single-page-app-with-flask-and-vuejs/
-'''
+"""
 
 import os
 from flask import Flask, request, jsonify, send_file
@@ -22,7 +22,7 @@ import shutil  # used to remove folder with stuff in it
 
 def extract_mouse_movements(log_file):
     coordinates = []
-    with open(log_file, 'r') as f:
+    with open(log_file, "r") as f:
         reader = csv.reader(f)
         next(reader)  # skips the header
         for row in reader:
@@ -38,7 +38,7 @@ def extract_mouse_movements(log_file):
 def create_heatmap(coordinates, screenshot_shape):
     heatmap = np.zeros(screenshot_shape[:2], dtype=np.float32)
 
-    for (x, y) in coordinates:
+    for x, y in coordinates:
         if 0 <= x < screenshot_shape[1] and 0 <= y < screenshot_shape[0]:
             heatmap[y, x] += 1
 
@@ -50,22 +50,25 @@ def create_heatmap(coordinates, screenshot_shape):
 def overlay_heatmap(heatmap, screenshot):
     heatmap_normalized = cv2.normalize(heatmap, None, 0, 255, cv2.NORM_MINMAX)
     heatmap_colored = cv2.applyColorMap(
-        heatmap_normalized.astype(np.uint8), cv2.COLORMAP_JET)
+        heatmap_normalized.astype(np.uint8), cv2.COLORMAP_JET
+    )
 
     # Combine the heatmap with the original screenshot
     overlay = cv2.addWeighted(screenshot, 0.7, heatmap_colored, 0.3, 0)
     return overlay
 
+
 # from the internet
 
 
 def zip_folder(folder_path, zip_name):
-    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 file_path = os.path.join(root, file)
                 arcname = os.path.relpath(file_path, folder_path)
                 zipf.write(file_path, arcname)
+
 
 # ***********************************************************************************
 
@@ -78,11 +81,19 @@ keyboard_tracking_thread = None
 def set_available_features(task_measurments):
     meaurement_id = []
     # makes sures the default taks are false
-    default_tasks = {'Mouse Movement': False, 'Mouse Scrolls': False,
-                     'Mouse Clicks': False, 'Keyboard Inputs': False}
+    default_tasks = {
+        "Mouse Movement": False,
+        "Mouse Scrolls": False,
+        "Mouse Clicks": False,
+        "Keyboard Inputs": False,
+    }
 
-    default_measurment_ids = {'Mouse Movement': 1, 'Mouse Scrolls': 2,
-                              'Mouse Clicks': 3, 'Keyboard Inputs': 4}
+    default_measurment_ids = {
+        "Mouse Movement": 1,
+        "Mouse Scrolls": 2,
+        "Mouse Clicks": 3,
+        "Keyboard Inputs": 4,
+    }
 
     # checks if features are in the array, then it will change hash into true
     for task in task_measurments:
@@ -97,6 +108,7 @@ def Mbox(title, text, style):
     # pop up function (https://stackoverflow.com/questions/2963263/how-can-i-create-a-simple-message-box-in-python)
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
+
 # 1. I get the Task id, task_direction, factor_id, participant_session_id from vue session form page, add these to the new app.py (TaskName_FactorName_MeasurementType_ParticipantSessionId.csv) -
 
 # 2. Randomization for task and factor combos
@@ -105,26 +117,67 @@ def Mbox(title, text, style):
 # 5. Download executable
 
 
-def get_measurments(study_name, user_Task, task_name, task_direction, task_duration, factor_name, random_factor_index, parti_id, study_id, user_task_id, default_measurment_ids, factor_ID):
+def get_measurments(
+    study_name,
+    user_Task,
+    task_name,
+    task_direction,
+    task_duration,
+    factor_name,
+    random_factor_index,
+    parti_id,
+    study_id,
+    user_task_id,
+    default_measurment_ids,
+    factor_ID,
+):
     # Start getting measurements from task
     for measurment_id in range(len(user_Task)):
         global mouse_tracking_thread, keyboard_tracking_thread
 
         # pop up
         if measurment_id == 0:
-            Mbox(f'{task_name[measurment_id]}',
-                 f'Directions: {task_direction[measurment_id]}\nFactor: {factor_name[random_factor_index]}\nDuration: {task_duration[measurment_id]} sec\n\nStart Task', 0)
+            Mbox(
+                f"{task_name[measurment_id]}",
+                f"Directions: {task_direction[measurment_id]}\nFactor: {factor_name[random_factor_index]}\nDuration: {task_duration[measurment_id]} sec\n\nStart Task",
+                0,
+            )
         else:
-            Mbox(f'{task_name[measurment_id]}',
-                 f'Directions: {task_direction[measurment_id]}\nFactor: {factor_name[random_factor_index]}\nDuration: {task_duration[measurment_id]} sec\n\nStart Next Task', 0)
+            Mbox(
+                f"{task_name[measurment_id]}",
+                f"Directions: {task_direction[measurment_id]}\nFactor: {factor_name[random_factor_index]}\nDuration: {task_duration[measurment_id]} sec\n\nStart Next Task",
+                0,
+            )
 
         # checks to see if any task was added
         if True in user_Task[measurment_id].values():
             start_time = time.time()  # when exeriment starts
 
-            if user_Task[measurment_id]['Mouse Movement'] is not False or user_Task[measurment_id]['Mouse Clicks'] is not False or user_Task[measurment_id]['Mouse Scrolls'] is not False or user_Task[measurment_id]['Keyboard Inputs'] is not False:
-                tracking_thread = threading.Thread(target=get_all_measurements, args=(study_name,
-                                                                                      task_duration[measurment_id], task_name[measurment_id], start_time, user_Task[measurment_id]['Keyboard Inputs'], user_Task[measurment_id]['Mouse Movement'], user_Task[measurment_id]['Mouse Clicks'], user_Task[measurment_id]['Mouse Scrolls'], factor_name[random_factor_index], parti_id, study_id, user_task_id[measurment_id], default_measurment_ids[measurment_id], factor_ID[measurment_id]))
+            if (
+                user_Task[measurment_id]["Mouse Movement"] is not False
+                or user_Task[measurment_id]["Mouse Clicks"] is not False
+                or user_Task[measurment_id]["Mouse Scrolls"] is not False
+                or user_Task[measurment_id]["Keyboard Inputs"] is not False
+            ):
+                tracking_thread = threading.Thread(
+                    target=get_all_measurements,
+                    args=(
+                        study_name,
+                        task_duration[measurment_id],
+                        task_name[measurment_id],
+                        start_time,
+                        user_Task[measurment_id]["Keyboard Inputs"],
+                        user_Task[measurment_id]["Mouse Movement"],
+                        user_Task[measurment_id]["Mouse Clicks"],
+                        user_Task[measurment_id]["Mouse Scrolls"],
+                        factor_name[random_factor_index],
+                        parti_id,
+                        study_id,
+                        user_task_id[measurment_id],
+                        default_measurment_ids[measurment_id],
+                        factor_ID[measurment_id],
+                    ),
+                )
                 tracking_thread.start()
                 app.logger.debug("tracking")
                 tracking_thread.join()
@@ -132,24 +185,27 @@ def get_measurments(study_name, user_Task, task_name, task_direction, task_durat
                 app.logger.debug("not tracking foo")
 
             # pop up
-            Mbox(f'{task_name[measurment_id]}', 'Task Ended', 0)
+            Mbox(f"{task_name[measurment_id]}", "Task Ended", 0)
 
             # this portion is from the code vinh sent us
-            if user_Task[measurment_id]['Mouse Movement'] is not False:
+            if user_Task[measurment_id]["Mouse Movement"] is not False:
                 # TAKES SCREENSHOT OF YOUR SCREEN
                 app.logger.debug("Capturing Screen! Please wait a moment...")
 
                 time.sleep(1)
                 screenshot = ImageGrab.grab()
                 screenshot.save(
-                    f"screenshot_{task_name[measurment_id]}_{factor_name[random_factor_index]}_{parti_id}.png")
+                    f"screenshot_{task_name[measurment_id]}_{factor_name[random_factor_index]}_{parti_id}.png"
+                )
 
                 screenshot = cv2.imread(
-                    f"screenshot_{task_name[measurment_id]}_{factor_name[random_factor_index]}_{parti_id}.png")
+                    f"screenshot_{task_name[measurment_id]}_{factor_name[random_factor_index]}_{parti_id}.png"
+                )
 
                 # Extract mouse movement coordinates
                 coordinates = extract_mouse_movements(
-                    f"./{study_name}/{task_name[measurment_id]}/{task_name[measurment_id]}_{factor_name[random_factor_index]}_Mouse Movement_{parti_id}_{study_id}_{user_task_id[measurment_id]}_{default_measurment_ids[measurment_id]}_{factor_ID[measurment_id]}_data.csv")
+                    f"./{study_name}/{task_name[measurment_id]}/{task_name[measurment_id]}_{factor_name[random_factor_index]}_Mouse Movement_{parti_id}_{study_id}_{user_task_id[measurment_id]}_{default_measurment_ids[measurment_id]}_{factor_ID[measurment_id]}_data.csv"
+                )
 
                 # Create the heatmap
                 heatmap = create_heatmap(coordinates, screenshot.shape)
@@ -159,14 +215,17 @@ def get_measurments(study_name, user_Task, task_name, task_direction, task_durat
 
                 # Save the output
                 cv2.imwrite(
-                    f"./{study_name}/{task_name[measurment_id]}/heatmap_{task_name[measurment_id]}.png", overlay)
+                    f"./{study_name}/{task_name[measurment_id]}/heatmap_{task_name[measurment_id]}.png",
+                    overlay,
+                )
 
                 # removes the initial screenshot
                 os.remove(
-                    f'./screenshot_{task_name[measurment_id]}_{factor_name[random_factor_index]}_{parti_id}.png')
+                    f"./screenshot_{task_name[measurment_id]}_{factor_name[random_factor_index]}_{parti_id}.png"
+                )
 
         else:
-            app.logger.debug('No Task added')
+            app.logger.debug("No Task added")
 
 
 # parses the factor data from the json
@@ -176,16 +235,16 @@ def parse_factor_data(data):
     factor_name = []
 
     # gets all the factor data
-    factor_data = data.get('factors', [])
+    factor_data = data.get("factors", [])
 
     # implement later, this is to randomly choose a task
     len_of_factors = len(factor_data)
     random_factor_index = random.randint(0, len_of_factors - 1)
 
     for i in range(len(factor_data)):
-        factor_desc.append(factor_data[i]['factorDescription'])
-        factor_ID.append(int(factor_data[i]['factorID']))
-        factor_name.append(factor_data[i]['factorName'])
+        factor_desc.append(factor_data[i]["factorDescription"])
+        factor_ID.append(int(factor_data[i]["factorID"]))
+        factor_name.append(factor_data[i]["factorName"])
 
     return factor_desc, factor_ID, factor_name, random_factor_index
 
@@ -199,30 +258,37 @@ def parse_task_data(data):
     task_id = []
     task_name = []
 
-    tasks_data = data.get('tasks', [])
+    tasks_data = data.get("tasks", [])
     # implement later, this is to randomly choose a task
     rand_tasks = sorted(tasks_data, key=lambda x: random.random())
 
     # gets all the factor data
     for i in range(len(rand_tasks)):
-        task_measurements.append(rand_tasks[i]['measurementOptions'])
-        task_descripton.append(rand_tasks[i]['taskDescription'])
-        task_direction.append(rand_tasks[i]['taskDirections'])
-        task_name.append(rand_tasks[i]['taskName'])
-        task_id.append(rand_tasks[i]['taskID'])
-        task_duration.append(float(rand_tasks[i]['taskDuration']))
+        task_measurements.append(rand_tasks[i]["measurementOptions"])
+        task_descripton.append(rand_tasks[i]["taskDescription"])
+        task_direction.append(rand_tasks[i]["taskDirections"])
+        task_name.append(rand_tasks[i]["taskName"])
+        task_id.append(rand_tasks[i]["taskID"])
+        task_duration.append(float(rand_tasks[i]["taskDuration"]))
 
-    return task_measurements, task_descripton, task_direction, task_duration, task_id, task_name
+    return (
+        task_measurements,
+        task_descripton,
+        task_direction,
+        task_duration,
+        task_id,
+        task_name,
+    )
 
 
 # gets the rest of the data from the json
 def parse_detail_data(data):
-    parti_count = data.get('participantCount')
-    study_desc = data.get('studyDescription')
-    study_dsgn_type = data.get('studyDesignType')
-    study_name = data.get('studyName')
-    parti_id = data.get('participantSessId')
-    study_id = data.get('study_id')
+    parti_count = data.get("participantCount")
+    study_desc = data.get("studyDescription")
+    study_dsgn_type = data.get("studyDesignType")
+    study_name = data.get("studyName")
+    parti_id = data.get("participantSessId")
+    study_id = data.get("study_id")
 
     return parti_count, study_desc, study_dsgn_type, study_name, parti_id, study_id
 
@@ -232,7 +298,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # enable CORS w/ specific routes
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/study_json", methods=["POST", "GET"])
@@ -240,10 +306,10 @@ def study_json():
     submissionData = request.get_json()
 
     json_object = json.dumps(submissionData, indent=4)
-    with open(f'../frontend/public/demo3.json', 'w') as f:
+    with open(f"../frontend/public/demo3.json", "w") as f:
         f.write(json_object)
 
-    return 'finished'
+    return "finished"
 
 
 # gets parameters from server and runs
@@ -255,49 +321,71 @@ def run_study():
     # with open('../frontend/public/demo3.json', 'r') as file:
     #     data = json.load(file)
     data = request.get_json()
-    app.logger.debug(f'{data}')
-    factor_desc, factor_ID, factor_name, random_factor_index = parse_factor_data(
-        data)
-    task_measurements, task_descripton, task_direction, task_duration, task_id, task_name = parse_task_data(
-        data)
-    parti_count, study_desc, study_dsgn_type, study_name, parti_id, study_id = parse_detail_data(
-        data)
+    app.logger.debug(f"{data}")
+    factor_desc, factor_ID, factor_name, random_factor_index = parse_factor_data(data)
+    (
+        task_measurements,
+        task_descripton,
+        task_direction,
+        task_duration,
+        task_id,
+        task_name,
+    ) = parse_task_data(data)
+    parti_count, study_desc, study_dsgn_type, study_name, parti_id, study_id = (
+        parse_detail_data(data)
+    )
 
     measurment_id = []
     # checks to see what rand_measurment were selected
     for task_amount in range(len(task_measurements)):
-        rand_measurment, rand_measurment_id = set_available_features(task_measurements
-                                                                     [task_amount])
+        rand_measurment, rand_measurment_id = set_available_features(
+            task_measurements[task_amount]
+        )
         # print(rand_measurment, rand_measurment_id)
         user_Task.append(rand_measurment)
         measurment_id.append(rand_measurment_id[0])
 
     # RECORDS EXPERIMENTS
-    get_measurments(study_name, user_Task, task_name, task_direction, task_duration, factor_name,
-                    random_factor_index, parti_id, int(study_id), task_id, measurment_id, factor_ID)
+    get_measurments(
+        study_name,
+        user_Task,
+        task_name,
+        task_direction,
+        task_duration,
+        factor_name,
+        random_factor_index,
+        parti_id,
+        int(study_id),
+        task_id,
+        measurment_id,
+        factor_ID,
+    )
 
     # turns folder to zip file
-    zip_file_name = f'{parti_id}.zip'
-    zip_folder(f'./{study_name}', zip_file_name)
-    shutil.rmtree(f'./{study_name}')
+    zip_file_name = f"{parti_id}.zip"
+    zip_folder(f"./{study_name}", zip_file_name)
+    shutil.rmtree(f"./{study_name}")
 
     return "finished"
+
 
 @app.route("/retrieve_zip/<zip_name>", methods=["GET"])
 def retrieve_zip(zip_name):
     z_path = os.path.join(os.getcwd(), zip_name)
-    if os.path.exists(z_path) and zip_name.endswith('.zip'):
+    if os.path.exists(z_path) and zip_name.endswith(".zip"):
         return send_file(z_path, as_attachment=True)
     else:
         return "", 200
-    
+
+
 @app.route("/retrieve_csv/<csv_name>", methods=["GET"])
 def retrieve_csv(csv_name):
     csv_path = os.path.join(os.getcwd(), csv_name)
-    if os.path.exists(csv_path) and csv_name.endswith('.csv'):
+    if os.path.exists(csv_path) and csv_name.endswith(".csv"):
         return send_file(csv_path, as_attachment=True)
     else:
-        return "", 404 
-    
+        return "", 404
+
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=5001, debug=True)
+    app.run(host="localhost", port=5001, debug=True)
--- local_backend/features/collect_all.py
+++ local_backend/features/collect_all.py
@@ -13,7 +13,7 @@ mouse_move_data = []
 mouse_click_data = []
 mouse_scroll_data = []
 
-'''***************************************************  Mouse Functions ***************************************************'''
+"""***************************************************  Mouse Functions ***************************************************"""
 # threshold is for the mouse movement, this determine whether it not it can record data and if the mouse has moved within a certain oiubt
 THRESHOLD = 10  # We have to change this incase we dont want the record every point
 PREV_XPOS = 0
@@ -30,7 +30,7 @@ prev_time = 0
 #         prev_time = curr_time
 #         print(f'2:curr time: {curr_time} | prev time: {prev_time}')
 
-'''
+"""
 Couple ways of doing this:
 - One way, using time to record the data points.
     Ex: every 1 second record a point
@@ -42,7 +42,8 @@ Couple ways of doing this:
     Ex: Record every 100 pixels
     pros: more data
     cons: creates a little lag
-'''
+"""
+
 
 def on_move(x, y):
     global PREV_XPOS, PREV_YPOS, curr_time, prev_time, running_time, mouse_move_data
@@ -50,12 +51,12 @@ def on_move(x, y):
     # distane will determine whether or not it should record the mouse movement
     # good technique is using the euclidian distance. Very well known in ML/AI and Robotics
     distance = euclidian_distance(x, y, PREV_XPOS, PREV_YPOS)
-    curr_time = datetime.now().strftime('%H:%M:%S')
+    curr_time = datetime.now().strftime("%H:%M:%S")
     # if the distance is larger than the threshold, then it can record
     if distance > THRESHOLD:  # and curr_time != prev_time:
 
         # inserts data into array
-        mouse_move_data.append([curr_time, '%.2f' % (time.time() - running_time), x, y])
+        mouse_move_data.append([curr_time, "%.2f" % (time.time() - running_time), x, y])
 
         PREV_XPOS = x
         PREV_YPOS = y
@@ -63,32 +64,78 @@ def on_move(x, y):
         # also takes time
         # prev_time = curr_time
 
+
 # not needed now, but this is from the original functions given
 def on_click(x, y, button, pressed):
     global running_time, mouse_click_data
-    mouse_click_data.append([datetime.now().strftime('%H:%M:%S'), '%.2f' % (time.time() - running_time), x, y])
+    mouse_click_data.append(
+        [
+            datetime.now().strftime("%H:%M:%S"),
+            "%.2f" % (time.time() - running_time),
+            x,
+            y,
+        ]
+    )
+
 
 def on_scroll(x, y, dx, dy):
     global running_time, mouse_scroll_data
-    mouse_scroll_data.append([datetime.now().strftime('%H:%M:%S'), '%.2f' % (time.time() - running_time), x, y])
+    mouse_scroll_data.append(
+        [
+            datetime.now().strftime("%H:%M:%S"),
+            "%.2f" % (time.time() - running_time),
+            x,
+            y,
+        ]
+    )
 
-'''************************************************************************************************************************'''
+
+"""************************************************************************************************************************"""
 
 
-'''***************************************************  Keyboard Functions ***************************************************'''
+"""***************************************************  Keyboard Functions ***************************************************"""
+
+
 def on_press(key):
     global keyboard_data, running_time
     try:
         keyboard_data.append(
-            [datetime.now().strftime('%H:%M:%S'), '%.2f' % (time.time() - running_time), key.char])
+            [
+                datetime.now().strftime("%H:%M:%S"),
+                "%.2f" % (time.time() - running_time),
+                key.char,
+            ]
+        )
     except AttributeError:
         keyboard_data.append(
-            [datetime.now().strftime('%H:%M:%S'), '%.2f' % (time.time() - running_time), key])
-        
-'''***************************************************************************************************************************'''
+            [
+                datetime.now().strftime("%H:%M:%S"),
+                "%.2f" % (time.time() - running_time),
+                key,
+            ]
+        )
+
+
+"""***************************************************************************************************************************"""
+
 
 # Gets all the measurments
-def get_all_measurements(study_name=None, run_time=10, task_name=None, exe_time=None, key_input_flag=False, move_flag=False, click_flag=False, scroll_flag=False, factor_name=0, parti_id=0, study_id=0, user_task_id=0, ran_measurment_id=0, factor_ID=0):
+def get_all_measurements(
+    study_name=None,
+    run_time=10,
+    task_name=None,
+    exe_time=None,
+    key_input_flag=False,
+    move_flag=False,
+    click_flag=False,
+    scroll_flag=False,
+    factor_name=0,
+    parti_id=0,
+    study_id=0,
+    user_task_id=0,
+    ran_measurment_id=0,
+    factor_ID=0,
+):
     global mouse_listener, key_listener, running_time, keyboard_data, running_time, mouse_move_data, mouse_click_data, mouse_scroll_data
     running_time = exe_time
 
@@ -96,34 +143,90 @@ def get_all_measurements(study_name=None, run_time=10, task_name=None, exe_time=
         mouse_listener = mouse.Listener(
             on_move=on_move if move_flag else None,
             on_click=on_click if click_flag else None,
-            on_scroll=on_scroll if scroll_flag else None
+            on_scroll=on_scroll if scroll_flag else None,
         )
 
     if key_listener is None or not key_listener.running:
         key_listener = keyboard.Listener(
-            on_press=on_press if key_input_flag else None,
-            on_release=None
+            on_press=on_press if key_input_flag else None, on_release=None
         )
         mouse_listener.start()
         key_listener.start()
 
         # time of execution
         start_time = time.time()
-        while mouse_listener.running and key_listener.running and (time.time() - start_time) < run_time:
+        while (
+            mouse_listener.running
+            and key_listener.running
+            and (time.time() - start_time) < run_time
+        ):
             None
 
         stop_mouse_ps()
         stop_keyboard_ps()
 
         # makes csv and inserts the data to the csv
-        create_csv(study_name, task_name, 'Keyboard Inputs', 'keyboard', keyboard_data, key_input_flag, factor_name, parti_id, study_id, user_task_id, ran_measurment_id, factor_ID)
-        create_csv(study_name, task_name, 'Mouse Movement', 'mouse', mouse_move_data, move_flag, factor_name, parti_id, study_id, user_task_id, ran_measurment_id, factor_ID)
-        create_csv(study_name, task_name, 'Mouse Clicks', 'mouse', mouse_click_data, click_flag, factor_name, parti_id, study_id, user_task_id, ran_measurment_id, factor_ID)
-        create_csv(study_name, task_name, 'Mouse Scrolls', 'mouse', mouse_scroll_data, scroll_flag, factor_name, parti_id, study_id, user_task_id, ran_measurment_id, factor_ID)
+        create_csv(
+            study_name,
+            task_name,
+            "Keyboard Inputs",
+            "keyboard",
+            keyboard_data,
+            key_input_flag,
+            factor_name,
+            parti_id,
+            study_id,
+            user_task_id,
+            ran_measurment_id,
+            factor_ID,
+        )
+        create_csv(
+            study_name,
+            task_name,
+            "Mouse Movement",
+            "mouse",
+            mouse_move_data,
+            move_flag,
+            factor_name,
+            parti_id,
+            study_id,
+            user_task_id,
+            ran_measurment_id,
+            factor_ID,
+        )
+        create_csv(
+            study_name,
+            task_name,
+            "Mouse Clicks",
+            "mouse",
+            mouse_click_data,
+            click_flag,
+            factor_name,
+            parti_id,
+            study_id,
+            user_task_id,
+            ran_measurment_id,
+            factor_ID,
+        )
+        create_csv(
+            study_name,
+            task_name,
+            "Mouse Scrolls",
+            "mouse",
+            mouse_scroll_data,
+            scroll_flag,
+            factor_name,
+            parti_id,
+            study_id,
+            user_task_id,
+            ran_measurment_id,
+            factor_ID,
+        )
         keyboard_data = []
         mouse_move_data = []
         mouse_click_data = []
         mouse_scroll_data = []
+
 
 def stop_mouse_ps():
     global mouse_listener
@@ -131,39 +234,56 @@ def stop_mouse_ps():
         mouse_listener.stop()
         mouse_listener = None
 
+
 def stop_keyboard_ps():
     global key_listener
     if key_listener is not None:
         key_listener.stop()
         key_listener = None
 
+
 temp_csv_counter = 1
 
-def create_csv(study_name, task_name, measur_name, feature, arr_data, is_used, factor_name, parti_id, study_id, user_task_id, ran_measurment_id, factor_ID):
+
+def create_csv(
+    study_name,
+    task_name,
+    measur_name,
+    feature,
+    arr_data,
+    is_used,
+    factor_name,
+    parti_id,
+    study_id,
+    user_task_id,
+    ran_measurment_id,
+    factor_ID,
+):
     global temp_csv_counter
-    
+
     if not is_used:
         return
 
     parent_path = os.path.join(study_name, task_name)
     os.makedirs(parent_path, exist_ok=True)
 
-    if feature == 'mouse':
-        df = pd.DataFrame(arr_data, columns=['Time', 'running_time', 'x', 'y'])
+    if feature == "mouse":
+        df = pd.DataFrame(arr_data, columns=["Time", "running_time", "x", "y"])
 
-    elif feature == 'keyboard':
-        df = pd.DataFrame(arr_data, columns=['Time', 'running_time', 'keys'])
+    elif feature == "keyboard":
+        df = pd.DataFrame(arr_data, columns=["Time", "running_time", "keys"])
 
-    task_path_name = os.path.join(parent_path, f'{task_name}_{factor_name}_{measur_name}_{parti_id}_{study_id}_{user_task_id}_{ran_measurment_id}_{factor_ID}_data.csv')
+    task_path_name = os.path.join(
+        parent_path,
+        f"{task_name}_{factor_name}_{measur_name}_{parti_id}_{study_id}_{user_task_id}_{ran_measurment_id}_{factor_ID}_data.csv",
+    )
     df.to_csv(task_path_name, index=False)
-    
+
     csv_name = f"{temp_csv_counter}.csv"
-    df.to_csv(csv_name,  index=False)
+    df.to_csv(csv_name, index=False)
     temp_csv_counter += 1
 
 
-    
-
 def euclidian_distance(x1, y1, x2, y2):
-    ed = ((x1-x2)**2 + (y1-y2)**2)**.5
+    ed = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
     return ed
