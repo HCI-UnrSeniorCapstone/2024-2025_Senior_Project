ca96a79e9d61c5842b33948e7704a2093bbe3d44
sharedhci <sharedhci@gmail.com>
2025-02-14 08:46:47 +0000

    Apply Black code formatting to: Update apply_python_formatter.yml

--- server_backend/app/__init__.py
+++ server_backend/app/__init__.py
@@ -7,29 +7,31 @@ import os
 
 mysql = MySQL()
 
+
 def create_app():
     # Creating app
     app = Flask(__name__)
     app.config.from_object(__name__)
-    
+
     # Load environment variables
     load_dotenv()
 
     # MYSQL Configuration
-    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+    app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+    app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+    app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+    app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
     mysql.init_app(app)
 
     # Server CSV pathway configuration
-    app.config['RESULTS_BASE_DIR_PATH'] = os.getenv('RESULTS_BASE_DIR_PATH')
+    app.config["RESULTS_BASE_DIR_PATH"] = os.getenv("RESULTS_BASE_DIR_PATH")
 
-    CORS(app, resources={r'/*': {'origins': '*'}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     # Register blueprints
     from app.routes.general import bp as general_bp
     from app.routes.studies import bp as studies_bp
+
     app.register_blueprint(general_bp)
     app.register_blueprint(studies_bp)
 
