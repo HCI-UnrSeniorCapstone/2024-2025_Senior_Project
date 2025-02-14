a9bdf5222dc9093a88065e26c75efb9e8c3fc671
Jarvis-Nicholas <97933530+Jarvis-Nicholas@users.noreply.github.com>
2025-02-13 23:59:49 -0800

    102 get zip files of csv data from server (#112)
    - fixed sample data of session data instance and factors
    - formatting fixes

--- server_backend/app/utility/studies.py
+++ server_backend/app/utility/studies.py
@@ -1,7 +1,18 @@
+import gzip
+import io
+import pandas as pd
+import json
+import os
+
+
 def set_available_features(task_measurments):
     # makes sures the default taks are false
-    default_tasks = {'Mouse Movement': False, 'Mouse Scrolls': False,
-                     'Mouse Clicks': False, 'Keyboard Inputs': False}
+    default_tasks = {
+        "Mouse Movement": False,
+        "Mouse Scrolls": False,
+        "Mouse Clicks": False,
+        "Keyboard Inputs": False,
+    }
 
     # checks if features are in the array, then it will change hash into true
     for task in task_measurments:
@@ -12,16 +23,24 @@ def set_available_features(task_measurments):
 
 
 def get_study_detail(subData):
-    study_name = subData.get('studyName')
-    study_desc = subData.get('studyDescription')
-    study_design = subData.get('studyDesignType')
-    people_count = subData.get('participantCount')
+    study_name = subData.get("studyName")
+    study_desc = subData.get("studyDescription")
+    study_design = subData.get("studyDesignType")
+    people_count = subData.get("participantCount")
 
-    return study_name, study_desc, study_design, 
+    return (
+        study_name,
+        study_desc,
+        study_design,
+    )
 
-def create_study_details(submissionData, conn, cur):
+
+def create_study_details(submissionData, cur):
     # Get study_design_type
-    cur.execute("SELECT study_design_type_id FROM study_design_type WHERE study_design_type_description = %s", (submissionData['studyDesignType'],))
+    cur.execute(
+        "SELECT study_design_type_id FROM study_design_type WHERE study_design_type_description = %s",
+        (submissionData["studyDesignType"],),
+    )
     result = cur.fetchone()
     study_design_type_id = result[0]
 
@@ -30,26 +49,30 @@ def create_study_details(submissionData, conn, cur):
     INSERT INTO study (study_name, study_description, study_design_type_id, expected_participants)
     VALUES (%s, %s, %s, %s)
     """
-    study_name = submissionData['studyName']
-    study_description = submissionData['studyDescription']
-    expected_participants = submissionData['participantCount']
+    study_name = submissionData["studyName"]
+    study_description = submissionData["studyDescription"]
+    expected_participants = submissionData["participantCount"]
 
     # Execute study insert
-    cur.execute(insert_study_query, (study_name, study_description, study_design_type_id, expected_participants))
-    
+    cur.execute(
+        insert_study_query,
+        (study_name, study_description, study_design_type_id, expected_participants),
+    )
+
     return cur.lastrowid
 
-def create_study_task_factor_details(study_id, submissionData, conn, cur):
+
+def create_study_task_factor_details(study_id, submissionData, cur):
     # Insert tasks
     insert_task_query = """
     INSERT INTO task (task_name, study_id, task_description, task_directions, duration)
     VALUES (%s, %s, %s, %s, %s)
     """
-    for task in submissionData['tasks']:
-        task_name = task['taskName']
-        task_description = task['taskDescription']
-        task_directions = task['taskDirections']
-        task_duration = task.get('taskDuration', None)
+    for task in submissionData["tasks"]:
+        task_name = task["taskName"]
+        task_description = task["taskDescription"]
+        task_directions = task["taskDirections"]
+        task_duration = task.get("taskDuration", None)
 
         # Error handling for bad duration inputs. Convert empty or invalid values to None
         if task_duration is not None:
@@ -59,7 +82,10 @@ def create_study_task_factor_details(study_id, submissionData, conn, cur):
                 task_duration = None
 
         # Execute insert for each task
-        cur.execute(insert_task_query, (task_name, study_id, task_description, task_directions, task_duration))
+        cur.execute(
+            insert_task_query,
+            (task_name, study_id, task_description, task_directions, task_duration),
+        )
 
         # Get the task_id of the newly inserted task
         task_id = cur.lastrowid
@@ -68,13 +94,13 @@ def create_study_task_factor_details(study_id, submissionData, conn, cur):
         select_measurement_option_query = """
         SELECT measurement_option_id FROM measurement_option WHERE measurement_option_name = %s
         """
-        # Define query for inserting task_measurement 
+        # Define query for inserting task_measurement
         insert_measurement_query = """
         INSERT INTO task_measurement (task_id, measurement_option_id)
         VALUES (%s, %s)
         """
         # Process each measurement option for the task
-        for measurement_option in task['measurementOptions']:
+        for measurement_option in task["measurementOptions"]:
             # Get id for each measurement option
             cur.execute(select_measurement_option_query, (measurement_option,))
             result = cur.fetchone()
@@ -85,11 +111,76 @@ def create_study_task_factor_details(study_id, submissionData, conn, cur):
                 cur.execute(insert_measurement_query, (task_id, measurement_option_id))
 
     # Insert factors
-    for factor in submissionData['factors']:
+    for factor in submissionData["factors"]:
         insert_factor_query = """
         INSERT INTO factor (study_id, factor_name, factor_description)
         VALUES (%s, %s, %s)
         """
-        factor_name = factor['factorName']
-        factor_description = factor['factorDescription']
-        cur.execute(insert_factor_query, (study_id, factor_name, factor_description,))
+        factor_name = factor["factorName"]
+        factor_description = factor["factorDescription"]
+        cur.execute(
+            insert_factor_query,
+            (
+                study_id,
+                factor_name,
+                factor_description,
+            ),
+        )
+
+
+def get_all_study_csv_files(study_id, cur):
+    # SQL query to get session data instance details
+    select_session_data_instance_routes_query = """
+        SELECT sdi.session_data_instance_id, sdi.csv_results_path, sdi.task_id, t.task_name, sdi.measurement_option_id, mo.measurement_option_name, sdi.factor_id, f.factor_name
+        FROM session_data_instance sdi
+        INNER JOIN participant_session ps
+        ON ps.participant_session_id = sdi.participant_session_id
+        INNER JOIN task AS t
+        ON t.study_id = ps.study_id AND t.task_id = sdi.task_id
+        INNER JOIN factor AS f
+        ON f.study_id = ps.study_id AND f.factor_id = sdi.factor_id
+        INNER JOIN measurement_option AS mo
+        ON mo.measurement_option_id = sdi.measurement_option_id
+        WHERE ps.study_id = %s
+        ORDER BY sdi.session_data_instance_id
+        """
+
+    cur.execute(select_session_data_instance_routes_query, (study_id,))
+
+    results = cur.fetchall()
+
+    # Sort the results by the size of the CSV files so that the user sees data quicker by loading smallest first
+    results_with_size = []
+    for result in results:
+        file_size = os.path.getsize(result[1])
+        results_with_size.append((result, file_size))
+
+    results_with_size.sort(key=lambda x: x[1])
+    return results_with_size
+
+
+def generate_session_data_from_csv(results_with_size, chunk_size):
+    for result, _ in results_with_size:
+        (
+            session_data_instance_id,
+            csv_path,
+            task_id,
+            _,
+            measurement_option_id,
+            _,
+            factor_id,
+            _,
+        ) = result
+        # Send metadata ONCE per session instance
+        metadata = {
+            "session_data_instance_id": session_data_instance_id,
+            "task_id": task_id,
+            "measurement_option_id": measurement_option_id,
+            "factor_id": factor_id,
+        }
+        yield json.dumps({"metadata": metadata}, separators=(",", ":")) + "\n"
+
+        # Read the CSV file in chunks
+        for chunk in pd.read_csv(csv_path, chunksize=chunk_size):
+            chunk_list = chunk.values.tolist()
+            yield json.dumps({"data": chunk_list}, separators=(",", ":")) + "\n"
