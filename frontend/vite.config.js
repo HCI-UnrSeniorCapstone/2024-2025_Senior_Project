948bfba1c2f97679808d376d8e048ac279fa8198
Jarvis-Nicholas <njarvis@unr.edu>
2025-04-23 14:53:35 -0700

    frontend development builds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, path.resolve(__dirname, '..'))
+
   const isProd = mode === 'production'
 
   const backendUrl = isProd
-    ? env.VITE_APP_PRODUCTION_BACKEND_URL || 'http://localhost'
-    : env.VITE_APP_DEVELOPMENT_BACKEND_URL || 'http://localhost'
+    ? env.VITE_APP_PRODUCTION_BACKEND_URL
+    : env.VITE_APP_DEVELOPMENT_BACKEND_URL
 
   const backendPort = isProd
-    ? env.VITE_APP_PRODUCTION_BACKEND_PORT || '8000'
-    : env.VITE_APP_DEVELOPMENT_BACKEND_PORT || '5002'
+    ? env.VITE_APP_PRODUCTION_BACKEND_PORT
+    : env.VITE_APP_DEVELOPMENT_BACKEND_PORT
+  if (!backendUrl || !backendPort) {
+    throw new Error(
+      'Backend URL or port is undefined — check your .env file and restart Vite!',
+    )
+  }
 
   const target = `${backendUrl}:${backendPort}`
   console.log(`[VITE] Proxying /api → ${target}`)
@@ -26,11 +33,11 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      host: '0.0.0.0', // allows VS Code SSH port forwarding
-      port: 5173,
+      // host: '0.0.0.0',
+      // port: 5173,
       proxy: {
         '/api': {
-          target,
+          target: target,
           changeOrigin: true,
           secure: false,
         },
