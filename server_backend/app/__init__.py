322982ed996234d68cfb15634f0b12deb9ae9e3f
Jarvis-Nicholas <njarvis006@gmail.com>
2025-04-23 15:22:40 -0700

    db fix

--- server_backend/app/__init__.py
+++ server_backend/app/__init__.py
@@ -47,6 +47,8 @@ def create_app(env="production"):
     app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
     app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
     app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+    app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+
     mysql.init_app(app)
 
     # Vue config
